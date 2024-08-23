2eabaa5b16404b42f83453294c251ee95bcb9201
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-08-23 20:06:06 +0530

    prog_3.cpp

--- prog_3.cpp
+++ prog_3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     
-    vector<int> arr = { 88, 76, 95, 89, 77, 84, 91, 73, 87, 82};
+    vector<int> arr = { 88, 76, 95, 89, 77, 84, 91, 73, 87, 82,33,12,56,90,71};
         
     int mini=arr[0];
     for(auto it:arr){
